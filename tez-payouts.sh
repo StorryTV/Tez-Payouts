723613695d55af09c3cdb708e19c148502b9468a
StorryTV <60582682+StorryTV@users.noreply.github.com>
2025-09-16 01:34:13 +0200

    Update tez-payouts.sh
    Added $SIGNER as a configurable option

--- tez-payouts.sh
+++ tez-payouts.sh
@@ -7,7 +7,8 @@ set -euo pipefail
 DRYRUN="--dry-run" # set to "--dry-run" for testing or set to "" for real payouts
 BAKER="tz1eo3APJNdYst3mu7axpyZsJBPEqwxm8Sv1" # Baker address
 PAYOUT="tz1Lz19xKSsczwGGZw7VkVkrN1x3xYZyGfts" # Payout address (if payouts come directly from the baker you can set this as "$BAKER"
-NODE="http://127.0.0.1:8732" # Tezos node RPC to send the transactions to
+SIGNER="http://localhost:6732" # Tezos remote/local signer (set to "" if you don't use a signer)
+NODE="http://127.0.0.1:8732" # Tezos node RPC
 API="https://api.mainnet.tzkt.io/v1" # TZKT api (for testnet you can use "https://api.ghostnet.tzkt.io/v1")
 
 STATEFILE="/var/lib/tezos/payout_state"   # track last paid cycle
@@ -95,7 +96,10 @@ done
 # -------------------------------
 # EXECUTE BATCH (Or DRY-RUN if enabled in the config above)
 # -------------------------------
-/usr/bin/octez-client --endpoint $NODE -R 'http://localhost:6732' multiple transfers from $PAYOUT using "$TMP" $DRYRUN
+if [ "$SIGNER" != "" ]; then
+    NODE="$NODE -R $SIGNER"
+fi
+/usr/bin/octez-client --endpoint $NODE multiple transfers from $PAYOUT using "$TMP" $DRYRUN
 
 # -------------------------------
 # SAVE STATE
